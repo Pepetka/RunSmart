f2719ea1c82b5a89fc89517385194a844a9e9c86
Vlad Kuznetsov <vlad.kznetsov@gmail.com>
2022-08-08 11:34:36 +0300

    fix some bugs and complete forms

--- src/js/script.js
+++ src/js/script.js
@@ -41,21 +41,7 @@ document.addEventListener("DOMContentLoaded", () => {
 		}
 	}
 
-	slides.style.left = "0px"
-
-	next.addEventListener("click", () => {
-		const slideWidth = slide.offsetWidth
-
-		if (slides.style.left.slice(0, -2) == -slideWidth * 2) {
-			slides.style.left = "0px"
-		} else {
-			slides.style.left = +slides.style.left.slice(0, -2) - slideWidth + "px"
-		}
-
-		dotChanger()
-	})
-
-	prev.addEventListener("click", () => {
+	function prevSlide() {
 		const slideWidth = slide.offsetWidth
 
 		if (slides.style.left.slice(0, -2) == 0) {
@@ -65,6 +51,28 @@ document.addEventListener("DOMContentLoaded", () => {
 		}
 
 		dotChanger()
+	}
+
+	function nextSlide() {
+		const slideWidth = slide.offsetWidth
+
+		if (slides.style.left.slice(0, -2) == -slideWidth * 2) {
+			slides.style.left = "0px"
+		} else {
+			slides.style.left = +slides.style.left.slice(0, -2) - slideWidth + "px"
+		}
+
+		dotChanger()
+	}
+
+	slides.style.left = "0px"
+
+	next.addEventListener("click", () => {
+		nextSlide()
+	})
+
+	prev.addEventListener("click", () => {
+		prevSlide()
 	})
 
 	dots.forEach((dot, i) => {
@@ -75,6 +83,27 @@ document.addEventListener("DOMContentLoaded", () => {
 
 			dotChanger()
 		})
+	})
+
+	let touchstartX = 0
+	let touchendX = 0
+
+	function checkDirection() {
+		if (touchendX < touchstartX) {
+			nextSlide()
+		}
+		if (touchendX > touchstartX) {
+			prevSlide()
+		}
+	}
+
+	slides.addEventListener("touchstart", (e) => {
+		touchstartX = e.changedTouches[0].screenX
+	})
+
+	slides.addEventListener("touchend", (e) => {
+		touchendX = e.changedTouches[0].screenX
+		checkDirection()
 	})
 
 	// catalog
@@ -270,7 +299,9 @@ document.addEventListener("DOMContentLoaded", () => {
 	showFiltered(cardsArr)
 	cardsEffect()
 
-	showHideModal("order")
+	document.querySelectorAll(".button[data-modal='order']").forEach((btn) => {
+		btn.addEventListener("click", (e) => showHideModal(e.target))
+	})
 
 	filterBtns.forEach((btn) => {
 		btn.addEventListener("click", () => {
@@ -279,82 +310,233 @@ document.addEventListener("DOMContentLoaded", () => {
 			activeBtn(btn)
 			showFiltered(cardsArr, btn.getAttribute("data-filter"))
 			cardsEffect()
-			showHideModal("order")
+			document.querySelectorAll(".button[data-modal='order']").forEach((btn) => {
+				btn.addEventListener("click", (e) => showHideModal(e.target))
+			})
 		})
 	})
 
 	// modals
 
-	function showHideModal(data) {
+	function getScrollbarWidth() {
+		const outer = document.createElement("div")
+		outer.style.visibility = "hidden"
+		outer.style.overflow = "scroll"
+		outer.style.msOverflowStyle = "scrollbar"
+		document.body.appendChild(outer)
+		const inner = document.createElement("div")
+		outer.appendChild(inner)
+		const scrollbarWidth = outer.offsetWidth - inner.offsetWidth
+		outer.parentNode.removeChild(outer)
+		return scrollbarWidth
+	}
+
+	function showModal(modal) {
+		const body = document.querySelector("body"),
+			up = document.querySelector(".up")
+
+		modal.style.display = "flex"
+		body.style.overflow = "hidden"
+		body.style.marginRight = `${getScrollbarWidth()}px`
+		up.style.marginRight = `${getScrollbarWidth()}px`
+	}
+
+	function hideModal(modal) {
+		const body = document.querySelector("body"),
+			up = document.querySelector(".up")
+
+		modal.style.display = "none"
+		body.style.overflow = ""
+		body.style.marginRight = "0px"
+		up.style.marginRight = "0"
+	}
+
+	function showHideModal(e) {
 		const modals = document.querySelectorAll(".modal"),
-			openBtns = document.querySelectorAll(`.button[data-modal=${data}]`),
-			closeBtns = document.querySelectorAll(".modal__close"),
-			body = document.querySelector("body"),
 			header = document.querySelector('.modal[data-modal="order"] .modal__header span')
 
-		function getScrollbarWidth() {
-			const outer = document.createElement("div")
-
-			outer.style.visibility = "hidden"
-			outer.style.overflow = "scroll"
-			outer.style.msOverflowStyle = "scrollbar"
-			document.body.appendChild(outer)
-
-			const inner = document.createElement("div")
-			outer.appendChild(inner)
-
-			const scrollbarWidth = outer.offsetWidth - inner.offsetWidth
-			outer.parentNode.removeChild(outer)
-
-			return scrollbarWidth
-		}
-
-		openBtns.forEach((btn) => {
-			btn.addEventListener("click", (e) => {
-				modals.forEach((modal) => {
-					modal.style.display = "none"
-
-					if (e.target.getAttribute("data-modal") == modal.getAttribute("data-modal")) {
-						if (btn.getAttribute("data-product")) {
-							header.textContent =
-								"Пульсометр " + btn.getAttribute("data-product").replaceAll("_", " ")
-						}
-
-						modal.style.display = "flex"
-						body.style.overflow = "hidden"
-						body.style.marginRight = `${getScrollbarWidth()}px`
-					}
-				})
-			})
-		})
-
 		modals.forEach((modal) => {
+			modal.style.display = "none"
+
+			if (e.getAttribute("data-modal") == modal.getAttribute("data-modal")) {
+				if (e.getAttribute("data-product")) {
+					header.textContent = "Пульсометр " + e.getAttribute("data-product").replaceAll("_", " ")
+				}
+
+				showModal(modal)
+			}
+
 			modal.addEventListener("click", (e) => {
-				closeBtns.forEach((btn) => {
-					if (e.target.querySelector(".modal__wrapper") || e.target === btn) {
-						modal.style.display = "none"
-						body.style.overflow = ""
-						body.style.marginRight = "0px"
-					}
-				})
+				if (
+					e.target.querySelector(".modal__wrapper") ||
+					e.target.classList.contains("modal__close")
+				) {
+					hideModal(modal)
+				}
 			})
 		})
 	}
 
-	showHideModal("consultation")
-	showHideModal("thanks")
+	document.querySelectorAll(".button[data-modal='consultation']").forEach((btn) => {
+		btn.addEventListener("click", (e) => showHideModal(e.target))
+	})
 
 	// forms
 
 	const forms = document.querySelectorAll(".form"),
-		inputs = document.querySelectorAll(".form__input"),
-		submitBtns = document.querySelectorAll(".button[data-modal='thanks']")
+		inputs = document.querySelectorAll(".form__input")
+
+	function formValidation(el) {
+		const formInputs = el.querySelectorAll(".form__input"),
+			inputInf = el.parentNode.querySelectorAll(".form__info"),
+			infContent = el.parentNode.querySelectorAll(".form__info-content")
+		const error = [true, true, true]
+
+		formInputs.forEach((input, i) => {
+			if (input.value.length === 0) {
+				infContent[i].textContent = "Заполните поле"
+				error[i] = true
+			} else if (input.value.length < 2 && i === 0) {
+				infContent[i].textContent = "Введите больше 2-ух символов"
+				error[i] = true
+			} else if (input.value.length !== 17 && i === 1) {
+				infContent[i].textContent = "Некорректный номер телефона"
+				error[i] = true
+			} else if (!input.value.includes("@") && i === 2) {
+				infContent[i].textContent = "Некорректный почтовый адрес"
+				error[i] = true
+			} else {
+				error[i] = false
+			}
+		})
+
+		for (let i = 0; i < 3; i++) {
+			if (error[i]) {
+				inputInf[i].style.display = "block"
+				formInputs[i].style.borderBottom = "solid 2px red"
+				formInputs[i].style.borderRight = "solid 2px red"
+			} else {
+				inputInf[i].style.display = "none"
+				formInputs[i].style.border = "none"
+			}
+		}
+
+		return error.reduce((previousValue, currentValue) => previousValue || currentValue)
+	}
+
+	document.querySelectorAll(".form__input[name='phone']").forEach((input) => {
+		input.addEventListener("focus", (e) => {
+			e.target.value = e.target.value.length ? e.target.value : "+7"
+		})
+
+		input.addEventListener("blur", (e) => {
+			e.target.value = e.target.value.length === 2 ? "" : e.target.value
+		})
+
+		input.addEventListener("input", (e) => {
+			const x = (
+				e.target.value.includes("+7")
+					? e.target.value.slice(2)
+					: e.target.value[0] == 8
+					? e.target.value.slice(1)
+					: e.target.value
+			)
+				.replace(/\D/g, "")
+				.match(/(\d{0,3})(\d{0,3})(\d{0,2})(\d{0,2})/)
+
+			e.target.value =
+				"+7" +
+				(!x[2] ? x[1] : "(" + x[1] + ") ") +
+				x[2] +
+				(x[3] ? "-" + x[3] : "") +
+				(x[4] ? "-" + x[4] : "")
+		})
+	})
+
+	inputs.forEach((input) => {
+		input.addEventListener("input", (e) => {
+			formValidation(e.target.parentNode.parentNode)
+		})
+	})
 
 	forms.forEach((form) => {
 		form.addEventListener("submit", (e) => {
 			e.preventDefault()
 
-			showHideModal("thanks")
+			if (!formValidation(e.target)) {
+				const formData = new FormData(e.target)
+
+				const text = e.target.querySelector(".button_form").textContent
+				e.target.querySelector(".button_form").textContent = "Отправка..."
+
+				fetch("mailer/smart.php", {
+					method: "POST",
+					body: formData,
+				})
+					.then((res) => {
+						const header = document.querySelector(".modal[data-modal='thanks'] .modal__header")
+						const textThanks = header.innerHTML
+
+						if (!res.ok) {
+							header.innerHTML = `Возникла непредвиденная ошибка!
+								<span>Проверьте подключение к интернету</span>`
+						}
+
+						showHideModal(e.target.querySelector(".button_form"))
+						setTimeout(() => {
+							forms.forEach(() => {
+								hideModal(document.querySelector(".modal[data-modal='thanks']"))
+								header.innerHTML = textThanks
+							})
+						}, 5000)
+
+						e.target.querySelector(".button_form").textContent = text
+						e.target
+							.querySelectorAll(".form__label")
+							.forEach((el) => el.classList.remove("form__label_active"))
+						e.target.reset()
+					})
+					.catch((error) => console.error(error))
+			}
 		})
+	})
+
+	inputs.forEach((input) => {
+		input.addEventListener("focus", (e) => {
+			e.target.parentNode.querySelector(".form__label").classList.add("form__label_active")
+		})
+
+		input.addEventListener("blur", (e) => {
+			e.target.value
+				? null
+				: e.target.parentNode.querySelector(".form__label").classList.remove("form__label_active")
+		})
+	})
+
+	// smooth scroll
+
+	const smoothLinks = document.querySelectorAll('a[href^="#"]'),
+		up = document.querySelector(".up")
+
+	for (let smoothLink of smoothLinks) {
+		smoothLink.addEventListener("click", function (e) {
+			e.preventDefault()
+			const id = smoothLink.getAttribute("href")
+
+			document.querySelector(id).scrollIntoView({
+				behavior: "smooth",
+				block: "start",
+			})
+		})
+	}
+
+	window.addEventListener("scroll", () => {
+		if (window.scrollY > 1000) {
+			up.style.visibility = "visible"
+			up.style.opacity = "1"
+		} else {
+			up.style.visibility = "hidden"
+			up.style.opacity = "0"
+		}
 	})
 })
