961d5e5edfe2db2cc7adb1fa3ca70687c37f1a37
Vlad Kuznetsov <vlad.kznetsov@gmail.com>
2022-08-08 13:57:11 +0300

    some fixes

--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -13,13 +13,13 @@ $mail->CharSet = 'utf-8';
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.mail.ru';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'vlad.kznetsov@mail.ru';                 // Наш логин
-$mail->Password = 'awn9E7hTkPtdyXX7vvRP';                           // Наш пароль от ящика
+$mail->Username = '';                 // Наш логин
+$mail->Password = '';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
 
-$mail->setFrom('vlad.kznetsov@mail.ru', 'Pulse');   // От кого письмо 
-$mail->addAddress('vlad.kuzntsov@mail.ru');     // Add a recipient
+$mail->setFrom('', 'Pulse');   // От кого письмо 
+$mail->addAddress('');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
